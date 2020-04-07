dfd645b3fe735419dad016f362dffabc2388c669
FILIPE S <filipe25322@gmail.com>
2020-04-07 12:51:33 -0300

    Implementada a árvore bináiria

--- src/main.c
+++ src/main.c
@@ -2,14 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "binary_tree.h"
+typedef struct node{
+    int code;
+    char info[50];
+    struct node *left, *right;
+}Node;
+
+int codGenerator(char str[]);
+Node *elementCreator(char str[]);
+Node *tree(Node *root, Node *element);
+void printInOrder(Node *root);
 
 int main(){
-    Node *nod = NULL;
+    Node *root = NULL;
 
-    nod->left = nod->right = NULL;
-    strcpy(nod->info, "Ola!");
-    puts("Hello Wolrd!");
+    Node *new_0 = elementCreator("chevrolet");
+    Node *new_1 = elementCreator("margarina");
 
-    printf("%s", nod->info);
+    root = tree(root, new_0);
+    root = tree(root, new_1);
+
+    printInOrder(root);
+    printf("\n");
+
+    return 0;
 }//END main
+
+int codGenerator(char str[]){
+    int sizeStr = strlen(str);
+    int counter = 0;
+
+    for(int i = 0; i < sizeStr; i++){
+        counter += str[i];
+    }
+
+    return counter;
+}//END function
+
+Node *elementCreator(char str[]){
+    Node *new = (Node*)malloc(sizeof(Node));
+    if(new == NULL) exit(EXIT_FAILURE);
+
+    new->left = new->right = NULL;
+    new->code = codGenerator(str);
+    strcpy(new->info, str);
+
+    return new;
+}//END function
+
+Node *tree(Node *root, Node *element){
+    if(root == NULL){
+        root = elementCreator(element->info);
+
+        return root;
+    }
+    else{
+        if(root->code > element->code)
+            root->left = tree(root->left, element);
+        else
+            root->right = tree(root->right, element);
+    }
+
+    return root;    
+}//END function
+
+void printInOrder(Node *root){
+    if(root == NULL)
+        return;
+    
+    printInOrder(root->left);
+    printf("[%d] ", root->code);
+    printInOrder(root->right);
+}//END funtion
